31843167c8d11e5bbdd00fac209ff3c7adbdd30e
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-13 10:17:19 -0700

    Finish color picker project

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
-console.log(color1.value);
+let body = document.querySelector('.color-picker');
+let h1 = document.querySelector('h1')
+
+
+let backgroundChanger = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value}`
+}
+
+color1.addEventListener('change', backgroundChanger)
+color2.addEventListener('change', backgroundChanger)
+
+
+
 
