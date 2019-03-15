63bcd6ca937737597799eaa3d2f03898c4da6d61
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-15 10:52:00 -0700

    Implement default setting

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDom from 'react-dom'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => { 
+    return(
+        <div>
+            Hello
+        </div>
+    );
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
