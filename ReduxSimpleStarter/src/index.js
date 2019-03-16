a75dbb6f6c911c53a4c9439444b91ed55ee0318c
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-16 01:34:49 -0700

    Create CONFIG_API_KEY

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
+import API_KEY from '../config_keys';
 
+const YT_API_KEY = API_KEY;
+console.log(YT_API_KEY);
 
 const App = () => { 
     return(
         <div>
+            {console.log(API_KEY)}
             <SearchBar />
         </div>
     );
