544ab9fb53a4b61813cb9afd67f38fe3a5c5e8be
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-18 14:02:36 -0700

    Insert dummy data

---
+++ react-complete-guide/src/Product/ProductData.js
@@ -0,0 +1,22 @@
+const products = [
+    { id: 1,
+      name: "Computer", 
+      description: "Non quis ullamco quis quis culpa sit ullamco in esse ad.",
+      price: 100
+    }, 
+    { id: 2,
+      name: "Camera", 
+      description: "Qui laboris veniam amet consectetur aute eiusmod ut qui occaecat.",
+      price: 200
+    },
+    { id: 3,
+      name: "Cell Phone", 
+      description: "Duis ullamco elit exercitation eiusmod cillum nulla Lorem fugiat irure cupidatat amet culpa ipsum.",
+      price: 500
+    },
+    { id: 4,
+      name: "Chicken", 
+      description: "Eiusmod id ad irure officia consequat.",
+      price: 18319
+    }
+
