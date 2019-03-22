787c2c2204aa87f2471df98ef3981f2c8ea9b6fd
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-21 17:53:58 -0700

    Implement Actions

---
+++ redux-counter/src/actions/index.js
@@ -0,0 +1,15 @@
+import * as types from './ActionTypes';
+
+export const increment = () => {
+    type: types.INCREMENT
+}
+
+export const decrement = () => {
+    type: types.DECREMENT
+}
+
+export const setColor = (color) => {
+    return {type: types.SET_COLOR,
+            color
+           }
+}
