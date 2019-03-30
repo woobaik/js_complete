4890a06c815d3cd666364e2431405ebb42a06574
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-03-29 22:20:45 -0700

    Finish redux thunk project

--- blog-for-redux-thunk/src/Actions/index.js
+++ blog-for-redux-thunk/src/Actions/index.js
@@ -1,9 +1,7 @@
 import jsonPlacholder from '../Apis/jsonPlaceholder';
 
-const fetchPostList = () => async dispatch => {
+export const fetchPostList = () => async dispatch => {
     const response = await jsonPlacholder.get('/posts')
 
     dispatch({ type: 'FETCH_POST', payload: response.data})
 }
-
-export default fetchPostList;
--- blog-for-redux-thunk/src/Components/PostList.js
+++ blog-for-redux-thunk/src/Components/PostList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import fetchPostList from '../Actions/';
+import { fetchPostList } from '../Actions/';
 
 class PostList extends Component {
 
